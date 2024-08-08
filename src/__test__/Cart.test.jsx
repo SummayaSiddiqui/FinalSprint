e28cfdd90c68428aad501f121aec0be89c976439
Hiba-Mohamed <hibamoha987@gmail.com>
2024-08-08 13:49:30 -0230

    cart tests complete

--- src/__test__/Cart.test.jsx
+++ src/__test__/Cart.test.jsx
@@ -1,10 +1,29 @@
 import { render, screen } from "@testing-library/react";
 import ShoppingCart from "../components/shoppingcart/ShoppingCart";
 import { MemoryRouter } from "react-router";
-import { ShoppingCartProvider } from "../components/shoppingcartcontext/ShoppingCartContext";
+import {
+  ShoppingCartProvider,
+  useShoppingCart,
+} from "../components/shoppingcartcontext/ShoppingCartContext";
 
+// Mock implementation of the useShoppingCart hook
+const mockUseShoppingCart = (cartItems) => () => ({
+  cartItems,
+});
+
+// Mock the entire module at the top level
+jest.mock("../components/shoppingcartcontext/ShoppingCartContext", () => {
+  const actualModule = jest.requireActual(
+    "../components/shoppingcartcontext/ShoppingCartContext"
+  );
+  return {
+    ...actualModule,
+    useShoppingCart: jest.fn(),
+  };
+});
 describe("CART COMPONENT", () => {
-  test('Renders Component Correctly', ()=>{
+  test("Renders Component Correctly", () => {
+    useShoppingCart.mockImplementation(mockUseShoppingCart([]));
     render(
       <MemoryRouter>
         <ShoppingCartProvider>
@@ -13,10 +32,11 @@ describe("CART COMPONENT", () => {
       </MemoryRouter>
     );
     const cartHeading = screen.getByRole("heading", { name: "Shopping Cart" });
-    expect(cartHeading).toBeInTheDocument()
-  
-  })
+    expect(cartHeading).toBeInTheDocument();
+  });
   test("Displays empty cart message when no items are in the cart", () => {
+    useShoppingCart.mockImplementation(mockUseShoppingCart([]));
+
     render(
       <MemoryRouter>
         <ShoppingCartProvider>
@@ -26,5 +46,43 @@ describe("CART COMPONENT", () => {
     );
     const emptyMessage = screen.getByText("Opps, Your Cart is Empty!");
     expect(emptyMessage).toBeInTheDocument();
+  });
+  test("Displays Cart Items When cart is not empty", () => {
+    useShoppingCart.mockImplementation(
+      mockUseShoppingCart([
+        {
+          id: 1,
+          name: "Sapiens: A Brief History of Humankind",
+          price: 19.99,
+          quantity: 1,
+          description:
+            "A book by Yuval Noah Harari that explores the history of humankind from the Stone Age to the present.",
+          image: "/images/sapiens.png",
+          summary:
+            "It offers a fascinating journey through the entirety of human history",
+        },
+        {
+          id: 2,
+          name: "Sapiens: A Brief History of Humankind",
+          price: 19.99,
+          quantity: 1,
+          description:
+            "A book by Yuval Noah Harari that explores the history of humankind from the Stone Age to the present.",
+          image: "/images/sapiens.png",
+          summary:
+            "It offers a fascinating journey through the entirety of human history",
+        },
+      ])
+    );
+    render(
+      <MemoryRouter>
+        <ShoppingCartProvider>
+          <ShoppingCart />
+        </ShoppingCartProvider>
+      </MemoryRouter>
+    );
+
+    const productCards= screen.getAllByRole('listitem')
+    expect(productCards).toHaveLength(2)
   });
 });
