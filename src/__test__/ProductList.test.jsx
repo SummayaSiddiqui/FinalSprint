4cc43f9681e345bdc762e177c2af8804a72d719d
Hiba-Mohamed <hibamoha987@gmail.com>
2024-08-08 12:52:09 -0230

    one more test added

--- src/__test__/ProductList.test.jsx
+++ src/__test__/ProductList.test.jsx
@@ -1,20 +1,46 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import ProductList from "../components/productlist/ProductList";
+import { getProducts } from "../../src/api";
 import { MemoryRouter } from "react-router";
 import { ShoppingCartProvider } from "../components/shoppingcartcontext/ShoppingCartContext";
 
 
-describe("PRODUCT LIST COMPONENT", ()=>{
-    test('Product List Renders Correctly', () => {
-         render(
-           <MemoryRouter>
-             <ShoppingCartProvider>
-               <ProductList />
-             </ShoppingCartProvider>
-           </MemoryRouter>
-         );
-         const productListHeading = screen.getByRole('heading', {name: /product list/i})
-         expect(productListHeading).toBeInTheDocument()
-        }
-        )
-})
+
+describe("PRODUCT LIST COMPONENT", () => {
+  test("Product List Renders Correctly", () => {
+    render(
+      <MemoryRouter>
+        <ShoppingCartProvider>
+          <ProductList />
+        </ShoppingCartProvider>
+      </MemoryRouter>
+    );
+    const productListHeading = screen.getByRole("heading", {
+      name: /product list/i,
+    });
+    expect(productListHeading).toBeInTheDocument();
+  });
+  it("fetches and displays products", async () => {
+   
+    const products = getProducts();
+
+    render(
+      <MemoryRouter>
+        <ShoppingCartProvider>
+          <ProductList />
+        </ShoppingCartProvider>
+      </MemoryRouter>
+    );
+
+    // Wait for the products to be fetched and rendered
+    await waitFor(() => {
+      products.forEach((product) => {
+        expect(screen.getByText(product.name)).toBeInTheDocument();
+        expect(screen.getByText(product.description)).toBeInTheDocument();
+        expect(
+          (screen.getAllByText(`$${product.price.toFixed(2)}`).length)
+        ).toBeGreaterThan(0);
+      });
+    });
+  });
+});
